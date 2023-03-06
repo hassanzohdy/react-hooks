64f857d18ee7763a77c06167f23890aa17b83401
hasan <hassanzohdy@gmail.com>
2023-03-06 08:10:33 +0200

    "Updated Dependencies"

--- src/hooks/useOnce.tsx
+++ src/hooks/useOnce.tsx
@@ -4,5 +4,11 @@ import React from "react";
  * This hook will call the given callback only once
  */
 export default function useOnce(callback: any) {
-  React.useEffect(callback, []);
+  const hasEffectBeenCalledRef = React.useRef(false);
+  React.useEffect(() => {
+    if (hasEffectBeenCalledRef.current) return;
+    const output = callback();
+    hasEffectBeenCalledRef.current = true;
+    return output;
+  }, []);
 }
